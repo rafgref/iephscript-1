ce07e520b21b87663e94253766a6ce9f6b2d1a92
lujertech <38028458+lujertech@users.noreply.github.com>
2018-04-04 11:30:56 +0800

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -4,7 +4,7 @@ read -p "username to be removed : " Pengguna
 
 if getent passwd $Pengguna > /dev/null 2>&1; then
         userdel $Pengguna
-        echo -e "User $Pengguna telah dihapus."
+        echo -e "User $Pengguna removed"
 else
-        echo -e "GAGAL: User $Pengguna tidak ada."
+        echo -e "FAILED: User $ User does not exist."
 fi
