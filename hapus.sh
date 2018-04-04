8a59f7668401aacef34da71ba0cfea7f012c25f8
lujertech <38028458+lujertech@users.noreply.github.com>
2018-04-04 10:57:35 +0800

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Nama user SSH yang akan dihapus : " Pengguna
+read -p "username to be removed : " Pengguna
 
 if getent passwd $Pengguna > /dev/null 2>&1; then
         userdel $Pengguna
