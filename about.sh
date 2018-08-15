0deb1613ad8fd4b35746ad866b3cec7f83a9af54
iephdevs <42347975+iephdevs@users.noreply.github.com>
2018-08-15 21:58:46 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "IEPH DEVELOPERS SCRIPT"
+echo -e ""
+echo -e "ADMIN"
+echo -e "•Rafael Mojico Grefal(PHC-Raf)"
+echo -e "•Yahj Limalliv Onabru(PHC-Jhay21)"
+echo -e "Ryan Dimaunahan(cyprocs24)"
+echo -e "•Jan Christian Lopena(Android Jc)"
+echo -e "================================================="
